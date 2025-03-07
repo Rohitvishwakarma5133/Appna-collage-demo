de134a2329c28ec1da6f8cd07f15320fea8e3618
Rohit Kumar <146556108+Rohitvishwakarma5133@users.noreply.github.com>
2025-03-07 19:54:36 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOpperations.js
@@ -0,0 +1,19 @@
+// let score="33";
+// console.log(typeof score);
+
+// let valueInNumber= Number(score);
+// console.log(typeof valueInNumber);
+
+// score="33abc";
+// console.log(typeof score);
+// valueInNumber=Number(score);
+// console.log(valueInNumber);
+let isLoggedIn=1;
+console.log(typeof isLoggedIn);
+let BooleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(BooleanIsLoggedIn);
+
+
+
+
+
