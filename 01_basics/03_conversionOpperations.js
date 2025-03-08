66dce7aa26175977418241ad41ffc2d7647e059b
Rohit Kumar <146556108+Rohitvishwakarma5133@users.noreply.github.com>
2025-03-08 17:02:29 +0000

    Comperison and operations

--- 01_basics/03_conversionOpperations.js
+++ 01_basics/03_conversionOpperations.js
@@ -8,10 +8,32 @@
 // console.log(typeof score);
 // valueInNumber=Number(score);
 // console.log(valueInNumber);
-let isLoggedIn=1;
-console.log(typeof isLoggedIn);
-let BooleanIsLoggedIn=Boolean(isLoggedIn);
-console.log(BooleanIsLoggedIn);
+// let isLoggedIn=1;
+// console.log(typeof isLoggedIn);
+// let BooleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(BooleanIsLoggedIn);
+
+//-------------------------------------------------Oprations------------------------------------
+let value=3;
+value=value**value;
+// console.log(value);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+false);
+// console.log(null);
+// console.log(undefined);
+console.log(+"A");
+console.log(NaN);
+
+
+
+
+
+
 
 
 
